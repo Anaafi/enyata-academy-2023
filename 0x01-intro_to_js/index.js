4d7d73b018ab2f9493370e1bbb6d06166614e772
Anaafi <raphaelanaafi5@gmail.com>
2023-05-25 23:24:14 +0000

    js assignment

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,23 @@
+
+let myName = 'Raphael'; //variable myName ia assign to my first name
+
+//check for data type
+console.log(typeof myName); //output: string
+
+//change value of myName to my surnama
+myName = 'Anaafi'; //output: Anaafi
+
+//car object with at least 10 properties
+let car = {
+    brand: 'Toyota',
+    model: 'hihglander',
+    year: '2023',
+    color: 'white',
+    mileage: '36mpg',
+    fuelType: 'Gasoline',
+    isAutomatic: true,
+    numOfDoors: 4,
+    features: ['Bluetooth', 'Backup Camera'],
+    ownerName: 'Raphael Anaafi'
+    
+}
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,20 @@
+//declaration of variable favoriteNumber
+const favoriteNumber = 10;
+
+//new variable halfOfFavNum is assign to favoriteNumber divided by 2
+const halfOfFavNum = favoriteNumber / 2; //output: 5
+
+
+//delaration of variable isaacsFavorite
+let isaacsFavorite = 13;
+
+//subtract my favorite number from Isaac's
+isaacsFavorite = isaacsFavorite - favoriteNumber //output: 3
+
+
+//Change the value of isaacsFavorite to be 26 times its current value
+isaacsFavorite = 26 * isaacsFavorite; //output: 78
+
+
+//Change the value of isaacsFavorite to the remainder of dividing the value of isaacsFavorite in question 5 by 3
+isaacsFavorite = isaacsFavorite % 3; //output: 0
